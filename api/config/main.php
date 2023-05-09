bc3a76d22237eb6988f1970fe2329caf2f21f416
Hellow <athellow@outlook.com>
2023-05-09 10:59:41 +0800

    响应json格式

--- api/config/main.php
+++ api/config/main.php
@@ -15,6 +15,16 @@ return [
         'request' => [
             'csrfParam' => '_csrf-api',
         ],
+        'response' => [
+            'class' => 'yii\web\Response',
+            'format' => \yii\web\Response::FORMAT_JSON,
+            'on beforeSend' => function ($event) {
+                $response = $event->sender;
+                if ($response->data !== null) {
+                    $response->statusCode = 200;
+                }
+            },
+        ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
