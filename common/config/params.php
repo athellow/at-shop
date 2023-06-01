a09ffc5ff9159e294484c5dbe69e10b41e9f3273
Hellow <athellow@outlook.com>
2023-06-01 21:52:24 +0800

    上传文件

--- common/config/params.php
+++ common/config/params.php
@@ -7,7 +7,25 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
 
+    'loacalFileDomain' => 'http://f.athellow.com',
+
     // 速率限制配置
     'api.ratelimit.limit' => 3,     // 允许的最大请求数
     'api.ratelimit.window' => 1,    // 窗口的大小，以秒为单位
+
+    
+
+    'storage' => [
+        'type' => 'local',
+        'drives' => [
+            'local' => [
+                'basePath' => Yii::getAlias('@frontend') . '/web',
+                'uploadFolder' => '/uploads',
+                'uploadTmpFolder' => '/uploads/tmp',
+                'uploadThumbFolder' => '/uploads/thumbs',
+                'domain' => 'http://f.athellow.com',
+            ]
+            
+        ],
+    ]
 ];
